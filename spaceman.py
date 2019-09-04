a9324be0221dec50bd8e35ab931dd379cc8b88e9
Tyler_Holland <tyler.holland@students.makeschool.com>
2019-09-04 13:55:32 -0700

    added all files and updated comments

---
+++ spaceman.py
@@ -0,0 +1,39 @@
+import random
+
+def load_word():
+    '''
+    A function that reads a text file of words and randomly selects one to use as the secret word
+        from the list.
+
+    Returns: 
+           string: The secret word to be used in the spaceman guessing game
+    '''
+    f = open('words.txt', 'r')
+    words_list = f.readlines()
+    f.close()
+
+    words_list = words_list[0].split(' ')
+    secret_word = random.choice(words_list)
+    return secret_word
+
+def is_word_guessed(secret_word, letters_guessed):
+    '''
+    A function that checks if all the letters of the secret word have been guessed.
+
+    Args:
+        secret_word (string): the random word the user is trying to guess.
+        letters_guessed (list of strings): list of letters that have been guessed so far.
+
+    Returns: 
+        bool: True only if all the letters of secret_word are in letters_guessed, False otherwise
+    '''
+    # TODO: Loop through the letters in the secret_word and check if a letter is not in lettersGuessed
+    pass
+
+def get_guessed_word(secret_word, letters_guessed):
+    '''
+    A function that is used to get a string showing the letters guessed so far in the secret word and underscores for letters that have not been guessed yet.
+
+    Args: 
+        secret_word (string): the random word the user is trying to guess.
+        letters_guessed (list of strings): list of letters that have been guessed so far.
